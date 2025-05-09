43d54915dde2ff232129493aaf77fa919273e48d
Brooklynn Stevenson <brooksteven580@gmail.com>
2025-05-08 22:11:04 -0400

    finished server.js

--- server.js
+++ server.js
@@ -17,3 +17,26 @@ const PORT = process.env.PORT || 3001 //// This is conditional assignment. PORT 
 app.use(express.urlencoded({ extended: true })) ////this allows us to have a req.body for whatever forms are submitted //for the checkout form where user enters personal information to check out
 app.use(express.json()) //this allows us to have a req.body whenever json data is submitted directly without a form, coming from reqbin or postman etc
 app.use(cors());
+
+app.use('/items', itemsRouter); //this is saying for all routes that start with /items look for them in itemsRouter
+app.use('/orders', ordersRouter);
+app.use('/users', usersRouter);
+app.use('/carts', cartsRouter)
+
+
+//define a base route
+app.get('/', (req, res) => {
+    res.send('Welcome to the Aritzia API!');
+});
+
+
+
+//start our server. listening on port 3000 or 3001
+app.listen(PORT, (req, res) => {
+    console.log(`Server is listening on PORT: ${PORT}`);
+});
+
+//now test ABT (always be testing)
+//npm run dev
+
+//Once we see that it's running we can now incorporate the connection to the database next
