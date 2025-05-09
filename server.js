69dae53c2c6b0998d60228cdd7338013c695feed
Brooklynn Stevenson <brooksteven580@gmail.com>
2025-05-09 16:16:43 -0400

    updated code with JWT in server.js and created new file auth-router.js which I exported to server.js and then imported it into server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import { config } from 'dotenv'; config(); //this is allowing the use of environment variables coming from a .env file
+import { config } from 'dotenv'; config(); //here we are loading .env variables into our process .env variable here. Also, into our process//this is allowing the use of environment variables coming from a .env file
 import './database/database.js' //import database connection into server.js
 import express from 'express' //this imports the express package
 import cors from 'cors';
@@ -40,3 +40,74 @@ app.listen(PORT, (req, res) => {
 //npm run dev
 
 //Once we see that it's running we can now incorporate the connection to the database next
+
+
+
+////////code from youtube video JWT Authentication Tutorial - Node.js ///////
+
+
+//this server is only for authentication
+
+
+
+// code from Youtube video JWT Authentication Tutorial - Node.js
+//QUESTION!!!! WHERE SHOULD THIS BE?
+import jwt from 'jsonwebtoken' 
+
+// app.use(express.json())
+
+//QUESTION!!!!! would this be the info in my controllers for cart? so all I need to do is do /carts below. Also which file should app.get('/carts, (req, res) be?)
+// const post = [
+//     {
+//         username: 'Kyle',
+//         title: 'Post 1'
+//     },
+//     {
+//         username: 'Jim',
+//         title: 'Post 2'
+//     }
+// ]
+
+//this will only return the post that the user has access to. do this for cart and if enough time order. it will identify with username, not userId_. so add username to each schema/controller your want the user to identify with. make username unique 
+app.get('/posts', (req, res) => {
+    res.json(post.filter(post => post.username === req.user.name))
+})
+
+
+//remove /login code
+
+// app.post('/login', (req, res) => {
+//     //authentication user here(watch video to do this)
+//     const username = req.body.usernmae
+//     const user = { name: username }
+//     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+//     res.json({ accessToken: accessToken }) //every time a user creates a username it will also get a accessToken
+// })
+
+
+
+//QUESTION!!!!!!! Should this below be in server.js?
+
+//here we are going to test our accessToken by doing this middleware which will authenticate our token
+function authenticateToken(req, res, next) {
+    //here we get the token from our header
+    const authHeader = req.headers['authorization']
+    //if we have a suthHeader then return the authHeader token portion which we split on the space otherwise return undefined
+    //so our token will either be undefined or it's going to be the actual token
+    const token = authHeader && authHeader.split('')[1]
+
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+     
+}
+
+// const jwt = require('jsonwebtoken')
+
+// app.listen(PORT, (req, res) => {
+//     console.log(`Server is listening on PORT: ${PORT}`);
+// });
